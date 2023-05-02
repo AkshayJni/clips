1969ed1adb51a3b27ee7c85a7b047e80b513a7b5
Akshay <akshayjani1999@gmail.com>
2023-05-02 23:12:02 +0530

    Reactie form tutorial completed.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -22,7 +22,7 @@ export class RegisterComponent {
   ]);
   password = new FormControl('',[
     Validators.required,
-    Validators.pattern('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm')
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
   ]);
   confirm_password = new FormControl('',[
     Validators.required
@@ -33,6 +33,10 @@ export class RegisterComponent {
     Validators.maxLength(13)
   ]);
 
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor ='blue';
+
   registerForm = new FormGroup({
     name: this.name,
     email: this.email,
@@ -41,4 +45,10 @@ export class RegisterComponent {
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
   });
+
+  register(){
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
+  }
 }
