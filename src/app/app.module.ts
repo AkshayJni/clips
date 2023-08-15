8ce4551f65f8e3fe83a537c0b374c02e408f461e
Akshay Jani <36702558+Akshay-Jani@users.noreply.github.com>
2023-08-15 21:41:31 +0530

    Revert "Init commit for file upload."

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
@@ -33,7 +32,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AngularFirestoreModule,
     VideoModule,
     AppRoutingModule,
-    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
